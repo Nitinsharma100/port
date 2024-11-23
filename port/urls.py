543771da048c7c673bc52d9940a7f9c7fa76eec9
unknown <ns651143@gmail.com>
2024-11-23 13:48:55 +0530

    add

--- port/urls.py
+++ port/urls.py
@@ -18,9 +18,18 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('portfolio.urls')),
+    path("", include('portfolio.urls')),
+]
 
-   
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# Static and Media URLs (for development mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
