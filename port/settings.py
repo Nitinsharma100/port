3cc8e0f5abcb5273b17fb1ba05f13d6853ef60f6
unknown <ns651143@gmail.com>
2024-11-23 12:45:43 +0530

    first

--- port/settings.py
+++ port/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sv@jxixi&d02(8o9c=op842-cu^*%8^o3$=#mrvz@bz*zq1v6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port.onrender.com']
 
 
 # Application definition
