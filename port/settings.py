8acdf743dd89fce31450b5b5fd00c441aa4c6731
unknown <ns651143@gmail.com>
2024-11-23 12:57:12 +0530

    second

--- port/settings.py
+++ port/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sv@jxixi&d02(8o9c=op842-cu^*%8^o3$=#mrvz@bz*zq1v6$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['port.onrender.com']
+ALLOWED_HOSTS = ['port.onrender.com', 'www.port.onrender.com']
 
 
 # Application definition
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
-STATIC_URL = 'static/'
-STATIC_FILES_DIRS=[os.path.join(BASE_DIR,'static/')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
